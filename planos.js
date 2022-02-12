4ddc436f2cc3bb42633d7f0c2d74848b2197932b
Amanda Alexandre <amanda.alexandre@outlook.com>
2022-02-11 23:23:30 -0300

    Animações jQuery nos cards dos planos

---
+++ planos.js
@@ -0,0 +1,31 @@
+$(function() {
+
+    $("#card1").on({
+        mouseenter: function() {
+            // alert("ok")
+            $("#card1").fadeTo(1000, 1.0);
+            $("#plano-titulo-prata").style("color", "#ffbb00");
+        },
+        mouseleave: function() {
+            // alert("fora")
+            $("#card1").fadeTo(1000, 0.4)
+        }
+    });
+
+    $("#card2").on({
+        mouseenter: function() {
+            // alert("ok")
+            $("#card2").fadeTo(1000, 1.0);
+            $("#plano-titulo-ouro").style("color", "#ffbb00");
+        },
+        mouseleave: function() {
+            // alert("fora")
+            $("#card2").fadeTo(1000, 0.4)
+        }
+    });
+
+
+
+
+
+})
