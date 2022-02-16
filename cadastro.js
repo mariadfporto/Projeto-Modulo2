972b82e858e43b5928e0491b4f4345d7b286d5a8
williamfirmino92 <89873481+williamfirmino92@users.noreply.github.com>
2022-02-16 15:37:49 -0300

    Add files via upload
    Atualizados os links e ícones do header e footer. Acerto de posicionamento de alguns elementos.

--- cadastro.js
+++ cadastro.js
@@ -52,10 +52,6 @@ $(document).ready(function(){
     produto = "pacote-ouro";
     console.log(produto)
    })
-
-   $("#botao-enviar").click(function() {
-    window.location.href = "cadastro-realizado-sucesso.html";
-   })
   
   });
 
